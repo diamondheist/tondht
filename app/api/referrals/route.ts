9d827694e67ffe6319c1140deac8cc9171afdd54
Jagaban <loomiedave@gmail.com>
2025-01-25 10:15:25 +0000

    ggg

--- app/api/referrals/route.ts
+++ app/api/referrals/route.ts
@@ -1,5 +1,5 @@
+import { NextRequest, NextResponse } from 'next/server';
 import { saveReferral, getReferrals, getReferrer } from '@/lib/referralService';
-import { NextRequest,NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
   try {
